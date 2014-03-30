14cad6fae6ce5874560df9478c7b0f82f289ac09
Aurélien Maury <amaury@xebia.fr>
2014-03-31 01:52:37 +0200

    maquette ok

--- js/app.js
+++
@@ -1,6 +0,0 @@
-$(function () {
-    $("nav a").click(function (event) {
-        event.preventDefault();
-        $('html,body').animate({scrollTop: $(this.hash).offset().top - 30}, 400);
-    });
-});
